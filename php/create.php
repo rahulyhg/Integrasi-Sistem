8e7c88f7872f784752a7302395c30028975a39e9
Aufa Billah <aufaroot18@gmail.com>
2019-01-14 09:40:16 +0700

    Create insert - Create

--- php/create.php
+++ php/create.php
@@ -12,7 +12,7 @@
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
 	<!-- Owm Style -->
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="../css/style.css">
 
 	<title>Document</title>
 </head>
@@ -20,7 +20,7 @@
 	<div class="container">
 		<div class="row head">
 			<div class="col-md-8">
-				<h2>Manage Inventory</h2>
+				<h2>Add Inventory</h2>
 			</div>
 			<div class="col-md-4 align text-right my-auto">
 				<a href="php/create.php"><button class="btn btn-success btn-sm">Add New Inventory</button></a>
@@ -28,54 +28,65 @@
 		</div>
 		<div class="row">
 			<div class="col-md-12">
-				<div class="table-responsive">
-					<table class="table table-hover">
-						<thead>
-							<tr>
-								<th>No</th>
-								<th class="w-50">Nama Barang</th>
-								<th class="w-50">Kategori</th>
-								<th>Jumlah</th>
-								<th>Actions</th>
-							</tr>
-						</thead>
-						<tbody>
-							<?php
-							$url = "https://webservice-demo.herokuapp.com/api/v1/barang/";
-							$data = file_get_contents($url);
-							$hasil = json_decode($data);
-							$no = 1;
-							foreach ($hasil->data as $hasil) {	
-								echo "<tr>";
-								// echo "<td>".$hasil->id."</td>";
-								echo "<td>".$no."</td>";
-								echo "<td>".$hasil->nama_barang."</td>";
-								echo "<td>".$hasil->kategori."</td>";
-								echo "<td>".$hasil->count."</td>";
-								echo "<td class='action'>";
-								echo '<span><a href="#"><span class="fas fa-edit"></span></a></span>';
-								echo '<span><a href="php/delete.php?id='.$hasil->id.'"><span class="fas fa-trash-alt"></span></a></span>';
-								echo "</td>";
-								$no++;
-							?>
-							<!-- <td class='text-center'>
-								<span><a href="#"><span class="fas fa-edit"></span></a></span>
-								<span><a href="php/delete.php?id=$hasil->id"><span class="fas fa-trash-alt"></span></a></span>
-							</td> -->
-							<?php
-								echo "<tr>";
-							}
-							?>
-						</tbody>
-					</table>
-				</div>
+				<form action="create.php" method="POST">
+					<div class="form-group">
+						<label>Nama Barang:</label>
+						<input class="form-control" name="namabarang" type="text">
+					</div>
+					<div class="form-group">
+						<label>Jumlah Barang:</label>
+						<input class="form-control" name="jumlahbarang" type="text">
+					</div>
+					<div class="form-group">
+						<label>Kategori Barang:</label>
+						<input class="form-control" name="kategoribarang" type="text">
+					</div>
+					<button type="submit" class="btn btn-primary">Submit</button>
+				</form>				
 			</div>
 		</div>
-	</div>	
-	
+	</div>
+
 	<!-- JavaScript Optional -->
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 </body>
 </html>
+
+<?php
+if (isset($_POST["namabarang"])) {
+	$namaBarang = $_POST["namabarang"];
+	$jumlahBarang = $_POST["jumlahbarang"];
+	$kategoriBarang = $_POST["kategoribarang"];
+	$url = "https://webservice-demo.herokuapp.com/api/v1/barang/?key=12345";
+
+
+	$ch = curl_init();	
+
+	$post = [
+		'name' => $namaBarang,
+		'count' => $jumlahBarang,
+		'id_kategori' => $kategoriBarang,
+	];
+
+	curl_setopt($ch, CURLOPT_URL, $url);
+
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+	curl_setopt($ch, CURLOPT_HEADER, false);
+
+	curl_setopt($ch, CURLOPT_POST, count($post));
+
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+
+	$output = curl_exec($ch);
+
+	curl_close($ch);
+
+	// echo $output;
+
+	header("Location: http://localhost/ISS/");
+	die();
+}
+?>
